ac8f7141f15f69e3b516c940e47f5e2ae7d55b5e
Ryan Milvenan <ryanmilvenan@gmail.com>
2017-06-05 20:46:23 -0600

    Fixed merge conflicts

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -63,9 +63,9 @@ module.exports = [{
         }
       }),
       new webpack.DefinePlugin({
-        'API': JSON.stringify('http://localhost:3000'),
         'AUTH0_DOMAIN': JSON.stringify('carnivalinparadise.auth0.com'),
         'AUTH0_CLIENT_ID': JSON.stringify('vtMxr0t5RYIhNY8ZqEyRQbuTHiPqeShn'),
+        'API': JSON.stringify('https://www.carnivalinparadise.com'),
         'process.env': {
           'NODE_ENV': JSON.stringify('production'),
         }
